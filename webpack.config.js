27a5b054632aedb3c15e52a9b8e2aeed2dfaceaa
abarmina <anutik-barmina@mail.ru>
2020-07-02 14:44:21 +0500

    configured webpack with sass and pug

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPugPlugin = require('html-webpack-pug-plugin');
+const extractSass = new ExtractTextPlugin({
+    filename: 'style.css',
+});
+  
+
+module.exports = {
+    mode: 'production',
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.pug$/,
+                use: [
+                    "raw-loader",
+                    "pug-html-loader"
+                ]
+            }
+        ]
+    },
+    plugins: [
+        extractSass,
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/index.pug',
+        }),
+        new HtmlWebpackPugPlugin()
+    ]
+};
