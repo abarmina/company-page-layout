010125bee56e60c562714e9cfe1b45440af06d07
abarmina <anutik-barmina@mail.ru>
2020-07-05 17:54:48 +0500

    restructuring

--- src/app.js
+++ src/app.js
@@ -3,4 +3,5 @@ import './styles/base.scss';
 import './styles/container.scss';
 import './styles/content.scss';
 import './styles/menu.scss';
+import './styles/nav.scss'
 import './styles/variables.scss'
