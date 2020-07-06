655502eb3de369c31c86d82b77992a61a6c4512d
abarmina <anutik-barmina@mail.ru>
2020-07-07 01:56:20 +0500

    install uglifyjs-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPugPlugin = require('html-webpack-pug-plugin');
 
 module.exports = {
